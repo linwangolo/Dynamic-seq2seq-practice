9c2ff227c72f007a1182b9208557b10bd8ea5e32
linwangolo <judy79802002@gmail.com>
2017-10-05 17:02:05 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -13,7 +13,7 @@ class preprocessing():
         self.decoderFile = './answer.txt'
         self.dictFile = 'word_dict.txt'
         jieba.load_userdict(self.dictFile)
-        self.stopwordsFile = "./preprocessing/stopwords.dat"     //
+        self.stopwordsFile = "./preprocessing/stopwords.dat"     #?
         
     def wordToVocabulary(self, originFile, vocabFile, segementFile):
         # stopwords = [i.strip() for i in open(self.stopwordsFile).readlines()]
@@ -24,24 +24,24 @@ class preprocessing():
         with open(originFile, 'r') as en:
             for sent in en.readlines():
                 # 去标点
-                if "enc" in segementFile:
+                if "enc" in segementFile:      # enc???
                     #sentence = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。“”’‘？?、~@#￥%……&*（）]+", "", sent.strip())
                     sentence = sent.strip()
                     words = jieba.lcut(sentence)
                     print(words)
                 else:
                     words = jieba.lcut(sent.strip())
-                vocabulary.extend(words)
+                vocabulary.extend(words)    
                 for word in words:
                     sege.write(word+" ")
-                sege.write("\n")
+                sege.write("\n")               # 將空白加入每句的字詞之間?
         sege.close()
 
         # 去重并存入词典
         vocab_file = open(vocabFile, "w")
         _vocabulary = list(set(vocabulary))
         _vocabulary.sort(key=vocabulary.index)
-        _vocabulary = self.vocab + _vocabulary
+        _vocabulary = self.vocab + _vocabulary     # self.vocab define?
         for index, word in enumerate(_vocabulary):
             vocab_file.write(word+"\n")
         vocab_file.close()
@@ -51,23 +51,23 @@ class preprocessing():
         vec = []
         with open(vocabFile, "r") as dict_f:
             for index, word in enumerate(dict_f.readlines()):
-                word_dicts[word.strip()] = index
+                word_dicts[word.strip()] = index                # build a dictionary of vocabularies
 
         f = open(doneFile, "w")
         with open(segementFile, "r") as sege_f:
             for sent in sege_f.readlines():
                 sents = [i.strip() for i in sent.split(" ")[:-1]]
-                vec.extend(sents)
+                vec.extend(sents)                                # vec FOR WHAT?
                 for word in sents:
-                    f.write(str(word_dicts.get(word))+" ")
+                    f.write(str(word_dicts.get(word))+" ")     # write a file for the corresponding values of the key words(str(value) transfer the number into string)
                 f.write("\n")
         f.close()
             
 
     def main(self):
         # 获得字典
-        self.wordToVocabulary(self.encoderFile, './preprocessing/enc.vocab', './preprocessing/enc.segement')
-        self.wordToVocabulary(self.decoderFile, './preprocessing/dec.vocab', './preprocessing/dec.segement')
+        self.wordToVocabulary(self.encoderFile, './preprocessing/enc.vocab', './preprocessing/enc.segement')    #
+        self.wordToVocabulary(self.decoderFile, './preprocessing/dec.vocab', './preprocessing/dec.segement')    # ??
         # 转向量
         self.toVec("./preprocessing/enc.segement", 
                    "./preprocessing/enc.vocab", 
